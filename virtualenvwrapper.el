d448380dfb1c4906162fbd081a502f12d422b369
James Porter <porterjamesj@gmail.com>
2013-09-21 17:07:28 -0500

    Update package documentation.

--- virtualenvwrapper.el
+++ virtualenvwrapper.el
@@ -20,9 +20,17 @@
 ;; - Implement the option to have eshell work in a separate
 ;;   namespace.  This would be a substantial refactor.
 ;; - Add an option for `venv-location' to be an alist.
-;; - Add autocompletion of virtualenvs at the eshell prompt.
-;; - Add ability to create and destroy multiple virtualenvs
-;;   at once.
+;; - Propertize the venv names in the output of `venv-lsvirtualenv'
+;;   so that clicking or pressing RET on one will switch to it.
+
+;;; VERSION HISTORY
+;; 20130921
+;; - Fix a bug that caused an error if exec-path was nil (Thanks Steven Huwig).
+;; - Fix a bug that prevented cpvirtualenv from working in eshell.
+;; - Fix a bug in which deleted virtualenvs sometimes still showed up
+;;   in completions.
+;; - Eshell commands now tab complete virtualenv names where appropriate.
+;; - mkvirtualenv and rmvirtualenv can now accept multiple names.
 
 
 ;;; Code:
