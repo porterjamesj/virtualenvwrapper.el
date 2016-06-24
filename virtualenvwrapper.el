porterjamesj/virtualenvwrapper.el

commit 3434dc7a43b963c507534adc4191ca9e559096d9

Add support to auto-activate virtualenv found in projetile-project-root
Now it is possible to automatically activate a virtualenv located in the
root of a projectile project (projectile-project-root) however this
isn't done magical, the user has to call `(venv-projectile-auto-workon)'
in some point after entering the project, i.e. via projectile-hooks,
python-mode hooks or the action `projectile-switch-project-action' which
is the recommended way and thus documented in the README.md.

By default it will detect only virtualenvs whose name is ".venv" but
the user can expand the options by settings all the desired names in
the `venv-dirlookup-names' list, this is all documented in the README.

virtualenvwrapper.el
@@ -40,6 +40,13 @@ The default location is ~/.virtualenvs/, which is where your virtualenvs
 are stored if you use virtualenvwrapper in the shell."
   :group 'virtualenvwrapper)
 
+(defcustom 'venv-dirlookup-names
+  '(".venv")
+  "Virtualenvs to search in the projectile-project-root
+to activate when one of them is found."
+  :type (repeat :tag "List of directories" file)
+  :group 'virtualenvwrapper)
+
 ;; hooks
 
 (defvar venv-premkvirtualenv-hook nil
@@ -93,6 +100,13 @@ are stored if you use virtualenvwrapper in the shell."
   "Set the system \\[pdb] command."
   (setq gud-pdb-command-name venv-system-gud-pdb-command-name))
 
+(defun venv-projectile-auto-workon ()
+  "If a venv in the projetile root exists, calls venv-workon it.
+Set your common venvs names in `venv-dirlookup-names'"
+  (let ((path (--first
+               (file-exists-p (concat (projectile-project-root) it))
+               venv-dirlookup-names)))
+    (when path (venv-workon path))))
 
 (defun venv-clear-history ()
   (setq venv-history nil))

commit d6e4401a19dd597a7523007b6f7dabc03872dce9

Fixes usage of `defcustom' macro
Removes bad quote placement and adds needed quote on the ":type" value.

This fixes build broken by 3434dc7a, see #33.

virtualenvwrapper.el
@@ -40,11 +40,11 @@ The default location is ~/.virtualenvs/, which is where your virtualenvs
 are stored if you use virtualenvwrapper in the shell."
   :group 'virtualenvwrapper)
 
-(defcustom 'venv-dirlookup-names
+(defcustom venv-dirlookup-names
   '(".venv")
   "Virtualenvs to search in the projectile-project-root
 to activate when one of them is found."
-  :type (repeat :tag "List of directories" file)
+  :type '(repeat file)
   :group 'virtualenvwrapper)
 
 ;; hooks